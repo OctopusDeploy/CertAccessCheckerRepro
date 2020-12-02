ffd445784dcf928288111dfba99d9efec0add7ef
Justin Walsh <49404281+Justin-Walsh@users.noreply.github.com>
2020-12-02 14:59:22 -0500

    Added source

---
+++ CertAccessChecker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace CertAccessChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hostingCertificates = GetCertificates("WebHosting");
+            var myCertificates = GetCertificates("My");
+            var certificates = new X509Certificate2Collection();
+            certificates.AddRange(hostingCertificates);
+            certificates.AddRange(myCertificates);
+            Console.WriteLine("Scan complete, added {0} certificates as valid.", certificates.Count);
+        }
+
+        static X509Certificate2Collection GetCertificates(string storeName)
+        {
+            var store = new X509Store(storeName, StoreLocation.LocalMachine);
+            store.Open(OpenFlags.ReadOnly);
+            var results = new X509Certificate2Collection();
+            foreach (var cert in store.Certificates)
+            {
+                Console.WriteLine("Found Certificate: {0}", cert.Subject);
+                if (cert.HasPrivateKey)
+                {
+                    Console.WriteLine("Has .HasPrivateKey set, adding as a valid option.");
+                    results.Add(cert);
+                }
+            }
+
+            store.Close();
+            return results;
+        }
+    }
+}
